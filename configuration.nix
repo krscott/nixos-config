072347ac4ddbeb9c17a145932feec4eebc7bcb18
Kris Scott <kscott91@gmail.com>
2024-02-15 08:40:45 -0600

    Add flake

--- configuration.nix
+++ configuration.nix
@@ -132,7 +132,5 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
 
-  nix.extraOptions = ''
-  experimental-features = nix-command flakes
-  '';
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
