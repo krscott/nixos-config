af1c3408d504570237942c646ccb0bb22dd424a9
Kris Scott <kscott91@gmail.com>
2024-02-14 22:46:30 -0600

    Add gitFull

--- configuration.nix
+++ configuration.nix
@@ -97,8 +97,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    autorandr
-    git
+    gitFull
     lshw
     vim
     wget
