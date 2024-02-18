3df73cdab8f0e8194ed935e15505e901d05ed11f
Kris Scott <kscott91@gmail.com>
2024-02-18 12:22:28 -0600

    Add git aliases

--- home.nix
+++ home.nix
@@ -1,9 +1,9 @@
 { config, pkgs, inputs, username,
   gtkThemeFromScheme, ... }:
-let 
+let
   inherit (import ./options.nix)
-    gitUsername gitEmail theme browser 
-    wallpaperDir wallpaperGit flakeDir 
+    gitUsername gitEmail theme browser
+    wallpaperDir wallpaperGit flakeDir
     waybarStyle;
 in {
   # Home Manager Settings
@@ -31,6 +31,20 @@ in {
     enable = true;
     userName = "${gitUsername}";
     userEmail = "${gitEmail}";
+    aliases = {
+      s = "status";
+      k = "!gitk --date-order";
+      ka = "!gitk --all --date-order";
+
+      # https://stackoverflow.com/questions/1838873/visualizing-branch-topology-in-git/34467298#34467298
+      lg = "lg1";
+      lg1 = "lg1-specific --all";
+      lg2 = "lg2-specific --all";
+      lg3 = "lg3-specific --all";
+      lg1-specific = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'";
+      lg2-specific = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
+      lg3-specific = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset) %C(bold cyan)(committed: %cD)%C(reset) %C(auto)%d%C(reset)%n''          %C(white)%s%C(reset)%n''          %C(dim white)- %an <%ae> %C(reset) %C(dim white)(committer: %cn <%ce>)%C(reset)'";
+    };
   };
 
   # Create XDG Dirs
